5270baa34c7dbdf5fbfc02ce99d059c47f49c554
Rohan-kumar <rohan.kumar.coding@gmail.com>
2026-01-01 17:49:16 +0900

    chore: delete Splitwise App implementation files including core classes and main entry point.

--- Splitwise App/EqualSplit.java
+++
@@ -1,6 +0,0 @@
-public class EqualSplit extends Split{
-    public EqualSplit(User user, double amount) {
-        super(user, amount);
-    }
-    
-}
--- Splitwise App/Expensetype.java
+++
@@ -1,3 +0,0 @@
-public enum Expensetype {
-    EQUAL,PERCENTAGE, EXACT, SHARE
-}
